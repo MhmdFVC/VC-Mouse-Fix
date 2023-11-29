ad0a66baf042bc61e65333d2731791122e0fec51
MhmdFVC <mhmd_fm@mailup.net>
2023-11-29 16:22:21 -0500

    Some cleanup

--- c/VC-Mouse-Fix.c
+++ c/VC-Mouse-Fix.c
@@ -1,4 +1,4 @@
-#define VERSION (*(unsigned char *)0x608578)
+#define VERSION (*(uint8_t*)0x608578)
 #define JP 0x44
 #define RETAIL_1_0 0x5D
 #define RETAIL_1_1 0x81
@@ -16,12 +16,12 @@ void Thread()
         case JP:
             sensResetAddr = 0x46F821;
             ySensFixTarget = 0x94ABD8;
-            ySensFixAddr1 = 0x479AC9;   // Sniper first-person aim
-            ySensFixAddr2 = 0x47A864;   // Rocket launcher first-person aim
-            ySensFixAddr3 = 0x47B3CC;  // M4/ruger first-person aim
-            ySensFixAddr4 = 0x47C496;  // Normal free aim    
-            ySensFixAddr5 = 0x48238A;  // "Runabout" (classic controls?)
-            nastyGameAddr = 0x68B110;         // nastyGame
+            ySensFixAddr1 = 0x479AC9;
+            ySensFixAddr2 = 0x47A864;
+            ySensFixAddr3 = 0x47B3CC;
+            ySensFixAddr4 = 0x47C496;   
+            ySensFixAddr5 = 0x48238A;
+            nastyGameAddr = 0x68B110;
             break;
         case RETAIL_1_0:
             ySensFixTarget = 0x94DBD0;    // Retail 1.0 only
@@ -33,7 +33,7 @@ void Thread()
             ySensFixAddr4 = 0x47C0BF;
             ySensFixAddr5 = 0x481FB3;
             nastyGameAddr = 0x68DD68;
-            if (VERSION == 0x5D) break;    // The above values also apply to 1.0 so I'm cheating a bit here
+            if (VERSION == RETAIL_1_0) break;   // The above values also apply to 1.0
             ySensFixTarget = 0x94DBD8;    // Retail 1.1 only
             break;
         case STEAM:
@@ -49,15 +49,13 @@ void Thread()
             break;
 	}
 
-    /*for (uint8_t i = 0; i < 10; i++)
-        SetChar(sensResetAddr++, NOP);*/
     for (uint32_t addrToWrite = sensResetAddr-1; addrToWrite < sensResetAddr+10; SetChar(++addrToWrite, NOP));
     SetInt(ySensFixAddr1, ySensFixTarget);  // Sniper first-person aim
     SetInt(ySensFixAddr2, ySensFixTarget);  // Rocket launcher first-person aim
     SetInt(ySensFixAddr3, ySensFixTarget);  // M4/ruger first-person aim
     SetInt(ySensFixAddr4, ySensFixTarget);  // Normal free aim    
     SetInt(ySensFixAddr5, ySensFixTarget);  // "Runabout" (classic controls?)
-    SetChar(nastyGameAddr, 1);         // nastyGame
+    SetChar(nastyGameAddr, 1);              // nastyGame
 
 	return;
 }
