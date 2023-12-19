362655c08b925175c88e37ad4bcf287eceef647c
MhmdFVC <mhmd_fm@mailup.net>
2023-12-19 16:55:57 -0500

    Don't set nastygame if initial lang is French/German
    Keeping the non-undesirable behavior of vanilla VC as it should be. Also added back in the memory protection changes when patching (using the function in Lighnat0r's y-sens reset fix) since now, compiling without it doesn't seem to work (though it did previously...)

--- c/VC-Mouse-Fix.c
+++ c/VC-Mouse-Fix.c
@@ -4,13 +4,18 @@
 #define RETAIL_1_1 0x81
 #define STEAM 0x5B
 #define GREEN_PEPPER 0xA1
+#define FRENCH 1
+#define GERMAN 2
 #include <stdint.h>
 #include <Windows.h>
 
+void patch(void* address, void* data, int size);
+
 void Thread()
 {
-    uint32_t sensResetAddr, ySensFixTarget, ySensFixAddr1, ySensFixAddr2, ySensFixAddr3, ySensFixAddr4, ySensFixAddr5, nastyGameAddr;
-    const uint8_t nop = 0x90, nastyGameEnabled = 1;
+    uint32_t sensResetAddr, ySensFixTarget, ySensFixAddr1, ySensFixAddr2, ySensFixAddr3, ySensFixAddr4, ySensFixAddr5, nastyGameAddr, langAddr;
+    uint8_t nop = 0x90, nastyGameEnabled = 1;
+    uint16_t nastyGameEnabled2 = 0x1B0;
 
 	switch (VERSION)
 	{
@@ -26,6 +31,7 @@ void Thread()
             break;
         case RETAIL_1_0:
             ySensFixTarget = 0x94DBD0;    // Retail 1.0 only
+            langAddr = 0x869680;
         case RETAIL_1_1:
             sensResetAddr = 0x46F4B1;
             ySensFixAddr1 = 0x4796F2;
@@ -50,15 +56,24 @@ void Thread()
             break;
 	}
 
-    for (uint32_t addrToWrite = sensResetAddr; addrToWrite < sensResetAddr+10; memcpy((void*)addrToWrite++, &nop, 1));
-    memcpy((void*)ySensFixAddr1, &ySensFixTarget, 4);  // Sniper first-person aim
-    memcpy((void*)ySensFixAddr2, &ySensFixTarget, 4);  // Rocket launcher first-person aim
-    memcpy((void*)ySensFixAddr3, &ySensFixTarget, 4);  // M4/ruger first-person aim
-    memcpy((void*)ySensFixAddr4, &ySensFixTarget, 4);  // Normal free aim    
-    memcpy((void*)ySensFixAddr5, &ySensFixTarget, 4);  // "Runabout" (classic controls?)
-    memcpy((void*)nastyGameAddr, &nastyGameEnabled, 1);// nastyGame
+    for (uint32_t addrToWrite = sensResetAddr; addrToWrite < sensResetAddr+10; patch((void*)addrToWrite++, &nop, 1));
+    patch((void*)ySensFixAddr1, &ySensFixTarget, 4);  // Sniper first-person aim
+    patch((void*)ySensFixAddr2, &ySensFixTarget, 4);  // Rocket launcher first-person aim
+    patch((void*)ySensFixAddr3, &ySensFixTarget, 4);  // M4/ruger first-person aim
+    patch((void*)ySensFixAddr4, &ySensFixTarget, 4);  // Normal free aim    
+    patch((void*)ySensFixAddr5, &ySensFixTarget, 4);  // "Runabout" (classic controls?)
+    if (*(uint8_t*)langAddr != FRENCH && *(uint8_t*)langAddr != GERMAN)
+        patch((void*)nastyGameAddr, &nastyGameEnabled, 1);// nastyGame -- only at game start, doesn't work for changing languages.
 
 	return;
+}
+
+void patch(void* address, void* data, int size)
+{
+	unsigned long protect[2];
+	VirtualProtect(address, size, PAGE_EXECUTE_READWRITE, &protect[0]);
+	memcpy(address, data, size);
+	VirtualProtect(address, size, protect[0], &protect[1]);
 }
 
 int APIENTRY DllMain(HMODULE hModule, DWORD reason, LPVOID lpReserved)
